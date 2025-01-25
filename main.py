23c3a464b7e1c42d051240ea6189df1ca451c002
Nathan3-14 <nathan.watson.172@gmail.com>
2025-01-25 10:49:55 +0000

    a

--- main.py
+++ main.py
@@ -54,3 +54,4 @@ test_data = BetterJson("data.json")
 exam_data = BetterJson("exams.json")
 
 print(get_week(["10/02", "11/02", "12/02", "13/02", "14/02"], exam_data))
+print(get_week(["24/02", "25/02", "26/02", "27/02", "28/02"], exam_data))
